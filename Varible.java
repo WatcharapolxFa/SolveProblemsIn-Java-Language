13005cba27e50b18c2c76f58c59c7d936fe8c90c
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-11 15:49:21 +0700

    full

--- Varible.java
+++ Varible.java
@@ -3,11 +3,13 @@ class Varible{
         // Global varible 
         final int number0 ; // Configure
         int number1 = 0 ; // None Configure
+        int number2 =500 ;
         number1 = 90;
         number0 = 50 ;
         //Out put
         System.out.println("ค่าตัวเลขชนิดที่ 1 : "+number1);
         System.out.println("ค่าตัวเลขชนิดที่ 2 : "+number0);
+        System.out.println("ค่าตัวเลข :"+number2);
         {
             // Local varible
             int number3 = 10;
