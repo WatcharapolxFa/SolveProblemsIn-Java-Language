b87e6b1fc37279ee38f5fb7ea806f1e00dc35e81
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-18 22:47:22 +0700

    full

--- Prepost.java
+++ Prepost.java
@@ -2,5 +2,24 @@ public class Prepost {
     public static void main(String[] args) {
         int a = 5;
 
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+
+        a = 5;
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+
+        int b = 5;
+        System.out.println(b);
+        System.out.println(b--);
+        System.out.println(b);
+
+        b = 5;
+        System.out.println(b);
+        System.out.println(--b);
+        System.out.println(b);
+
     }
 }
