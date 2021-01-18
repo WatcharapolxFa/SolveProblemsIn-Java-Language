651d62d15dcf5911db58617c348eb5e6169e7250
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-18 23:46:32 +0700

    full

--- Self learning/Condition.java
+++ Self learning/Condition.java
@@ -1,7 +1,9 @@
 public class Condition {
     public static void main(String[] args) {
         boolean result = 10 > 5;
-        System.out.println(result);
+        if (result) {
+            System.out.println("10 > 5 ");
+        }
     }
 
 }
