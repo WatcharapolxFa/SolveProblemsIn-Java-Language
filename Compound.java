8e538a29fa79c5443a2b63016ca62c4a70a383c1
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-18 23:06:03 +0700

    full

---
+++ Compound.java
@@ -0,0 +1,16 @@
+public class Compound {
+    public static void main(String[] args) {
+        int x = 10, y = 5;
+        System.out.println("ก่อน บวกค่า " + x);
+        x += y;
+        System.out.println("หลัง บวกค่า " + x);
+        x -= y;
+        System.out.println("หลัง ลบค่า " + x);
+        x *= y;
+        System.out.println("หลัง คูณค่า " + x);
+        x /= y;
+        System.out.println("หลัง หารค่า " + x);
+
+    }
+
+}
