f2544d9cdc45ad26e874843487e8993c06f0570c
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-18 23:52:51 +0700

    full

--- Self learning/Condition.java
+++ Self learning/Condition.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Condition {
     public static void main(String[] args) {
-        boolean result = 10 > 5;
-        if (result) {
-            System.out.println("10 > 5 ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ป้อนอายุของนาย ท่าน : ");
+        int age = scanner.nextInt();
+        if (age >= 15) {
+            System.out.print("นาย / นางสาว : " + age);
         }
+        System.out.print("จบโปรแกรม ");
     }
 
 }
