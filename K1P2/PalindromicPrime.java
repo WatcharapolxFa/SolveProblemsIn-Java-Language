0f47d1d540b850a937484f71002496858109fa2a
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-26 16:49:27 +0700

    full

--- K1P2/PalindromicPrime.java
+++ K1P2/PalindromicPrime.java
@@ -23,15 +23,15 @@ public class PalindromicPrime {
 
     }
 
-    public static boolean isPrime(int number) {
-        number = Math.abs(number);
-        if (number == 2) {
+    public static boolean isPrime(int num) {
+        num = Math.abs(num);
+        if (num == 2) {
             return true;
-        } else if (number % 2 == 0 || number == 1) {
+        } else if (num % 2 == 0 || num == 1) {
             return false;
         } else {
-            for (int i = 3; i <= Math.sqrt(number); i += 2) {
-                if (number % i == 0) {
+            for (int i = 3; i <= Math.sqrt(num); i += 2) {
+                if (num % i == 0) {
                     return false;
                 }
             }
