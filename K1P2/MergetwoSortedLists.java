86d80c96be9c1bc3bbce82b81731863f5c9ab218
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-01-26 17:56:06 +0700

    full

--- K1P2/MergetwoSortedLists.java
+++ K1P2/MergetwoSortedLists.java
@@ -1,13 +1,51 @@
 import java.util.Random;
 
 public class MergetwoSortedLists {
-    public static void main(String[] args) {
 
+    /**
+     * @param args the command line arguments
+     */
     void merge(int arr[], int l, int m, int r) {
         // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
+        // Create temp arrays /
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data to temp arrays/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        // Merge the temp arrays
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
         while (j < n2) {
             arr[k] = R[j];
             j++;
@@ -61,36 +99,34 @@ public class MergetwoSortedLists {
         }
         System.out.println();
     }
-    public static int[] merge(int[] list1, int[] list2){
+
+    public static int[] merge(int[] list1, int[] list2) {
         int leftLen = list1.length;
         int rightLen = list2.length;
-        int[] sortedList = new int[leftLen+rightLen];
-        int sortedIndex = 0, left=0, right=0;
+        int[] sortedList = new int[leftLen + rightLen];
+        int sortedIndex = 0, left = 0, right = 0;
 
-
-        while(left < leftLen && right < rightLen){
-            if(list1[left] < list2[right]){
+        while (left < leftLen && right < rightLen) {
+            if (list1[left] < list2[right]) {
                 sortedList[sortedIndex] = list1[left];
                 left++;
-            }
-            else{
+            } else {
                 sortedList[sortedIndex] = list2[right];
                 right++;
             }
             sortedIndex++;
         }
 
-        while(left < leftLen){
+        while (left < leftLen) {
             sortedList[sortedIndex] = list1[left];
             left++;
             sortedIndex++;
         }
-        while(right < rightLen){
+        while (right < rightLen) {
             sortedList[sortedIndex] = list2[right];
             right++;
             sortedIndex++;
         }
         return sortedList;
-    }
     }
 }
