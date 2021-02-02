fad741bdd96fd6a35420263f07323769d913d3bd
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-02-02 14:00:46 +0700

    full

---
+++ K1P2/TestOop1.java
@@ -0,0 +1,10 @@
+import Oop1.Circle;
+
+public class TestOop1 {
+
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        Circle c2 = new Circle(2.5, "red");
+    }
+
+}
