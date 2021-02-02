c2d7e9907120d01d4a9ec679ffa2c66edf145eb6
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-02-02 13:45:57 +0700

    full

--- K1P2/Oop1.java
+++ K1P2/Oop1.java
@@ -7,21 +7,19 @@ public class Oop1 {
         double radius;
         String color;
 
-    }
-
-    Circle(){
-            radius = 1.0 ;
+        Circle() {
+            radius = 1.0;
             color = "while";
         }
 
-    Circle(double newReadius , String newColor)
-        {
+        Circle(double newReadius, String newColor) {
             radius = newReadius;
             color = newColor;
         }
 
-    double getArea() {
-        return Math.PI * Math.pow(radius, 2);
+        double getArea() {
+            return Math.PI * Math.pow(radius, 2);
+        }
     }
 
 }
