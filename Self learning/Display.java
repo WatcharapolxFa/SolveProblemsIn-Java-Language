46e887d37dc8e79d9f5c78194ddbd6e005df3884
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-13 17:30:24 +0700

    100 PI

---
+++ Self learning/Display.java
@@ -0,0 +1,7 @@
+public class Display {
+    public static void main(String[] args) {
+        System.out.print(" Name  : Watcharapol ");
+        System.out.println(" Last name : Yotadee");
+        System.out.print("Nickname : Far  ");
+    }
+}
