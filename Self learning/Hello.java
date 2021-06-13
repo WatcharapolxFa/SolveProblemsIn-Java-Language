2997a0c6d798de23dee0483e14f84356d5098345
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-13 14:35:33 +0700

    0

--- Self learning/Hello.java
+++ Self learning/Hello.java
@@ -1,7 +1,6 @@
- class Hello {
-        public static void main(String[] args) {
-            System.out.println("Hello Watcharapol Yotadee");//Show new line results
-            System.out.print("Hi FlxkexFa");//Show line
-        }
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Watcharapol Yotadee");// Show new line results
+        System.out.print("Hi Far");// Show line
     }
-    
+}
