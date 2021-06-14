0813d2da8218145c5e9810e11791a69c8d2e2e84
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-14 23:32:37 +0700

    100

--- Self learning/Condition.java
+++ Self learning/Condition.java
@@ -10,13 +10,21 @@ public class Condition {
         // In Put
         System.out.print("ป้อนเบอร์โทรศัพท์: ");
         String call = scanner.nextLine();
+        // In Put
+        System.out.print("ปีเกิดของท่าน(พ.ศ.): ");
+        int year = scanner.nextInt();
+
+        int age = 2564 - year;
         // Out Put
         System.out.println("ชื่อของท่าน : " + name);
         System.out.println("เบอร์โทรศัพท์ของท่าน : " + call);
-        // if (age >= 15) {
-        // System.out.println("นาย / นางสาว : " + age);
-        // }
-        // System.out.print("จบโปรแกรม ");
+        System.out.println("ปีเกิดของท่าน(พ.ศ.): " + year);
+        System.out.println("อายุของท่าน: " + age);
+
     }
 
 }
+// if (age >= 15) {
+// System.out.println("นาย / นางสาว : " + age);
+// }
+// System.out.print("จบโปรแกรม ");
