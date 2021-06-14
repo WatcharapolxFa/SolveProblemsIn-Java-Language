63f2f03ca8a6af9af94ac838d1c88f46761b3737
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-14 21:20:23 +0700

    100

--- Self learning/Varible.java
+++ Self learning/Varible.java
@@ -1,21 +1,34 @@
-class Varible{
+class Varible {
     public static void main(String[] args) {
-        // Global varible 
-        final int number0 ; // Configure
-        int number1 = 0 ; // None Configure
-        int number2 =500 ;
+
+        int num1;
+        float num2;
+        char alphabet;
+
+        int numnum1 = 10;
+        float numnum2 = 20;
+        char alphabets = 'F';
+
+        double z = 0.00, x = 10.0, v = 25.0;
+
+        // System.out.println("ค่าตัวเลขชนิดที่ 1 : " + numnum2);
+
+        // Global varible
+        final int number0; // Configure
+        int number1 = 0; // None Configure
+        int number2 = 500;
         number1 = 90;
-        number0 = 50 ;
-        //Out put
-        System.out.println("ค่าตัวเลขชนิดที่ 1 : "+number1);
-        System.out.println("ค่าตัวเลขชนิดที่ 2 : "+number0);
-        System.out.println("ค่าตัวเลข :"+number2);
+        number0 = 50;
+        // Out put
+        System.out.println("ค่าตัวเลขชนิดที่ 1 : " + number1);
+        System.out.println("ค่าตัวเลขชนิดที่ 2 : " + number0);
+        System.out.println("ค่าตัวเลข :" + number2);
         {
             // Local varible
             int number3 = 10;
-            System.out.println("ค่าตัวเลขชนิดที่ 3 : "+number3);
+            System.out.println("ค่าตัวเลขชนิดที่ 3 : " + number3);
         }
-        
-    }   
+
+    }
 
 }
