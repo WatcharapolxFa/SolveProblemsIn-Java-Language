99425c2258dc872fc33f173c3e8ddf82e74b9795
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-16 22:22:42 +0700

    100

--- K1P2/K1P2/Test01/Hellos.java
+++
@@ -1,9 +0,0 @@
-
-public class Hellos {
-    public static void main(String[] args) {
-        System.out.println("Watcharapol");
-        System.out.print("Yotadee");
-        System.out.print("Far");
-
-    }
-}
