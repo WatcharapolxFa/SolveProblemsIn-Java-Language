8c9c09472e2d810062873160eec4339d0b641627
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-17 21:27:57 +0700

    100

--- Self learning/Ifstatement.java
+++ Self learning/Ifstatement.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Ifstatement {
     public static void main(String[] args) {
-        boolean sum = 10 > 5;
-        if (sum == true) {
-            System.out.println("10 มากกว่า 5 ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ป้อนอายุของคุณ : ");
+        int age = scanner.nextInt();
+
+        if (age >= 15) {
+            System.out.println("คำนำหน้าของคุณคือ นาย/นางสาว ");
+        } else {
+            System.out.println("คำนำหน้าของคุณคือ เด็กชาย/เด็กหญิง ");
         }
     }
 }
