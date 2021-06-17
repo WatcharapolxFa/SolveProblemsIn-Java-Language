0ba41b1d07a3722a50741ccf3571d17b655b74e1
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-17 18:07:07 +0700

    100

---
+++ Self learning/Ifstatement.java
@@ -0,0 +1,8 @@
+public class Ifstatement {
+    public static void main(String[] args) {
+        boolean sum = 10 > 5;
+        if (sum == true) {
+            System.out.println("10 มากกว่า 5 ");
+        }
+    }
+}
