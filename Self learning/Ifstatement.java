10e1356371f0563902b1f4de4689295769be86f6
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-17 22:10:33 +0700

    100

--- Self learning/Ifstatement.java
+++ Self learning/Ifstatement.java
@@ -6,10 +6,16 @@ public class Ifstatement {
         System.out.print("ป้อนอายุของคุณ : ");
         int age = scanner.nextInt();
 
-        if (age >= 15) {
-            System.out.println("คำนำหน้าของคุณคือ นาย/นางสาว ");
-        } else {
-            System.out.println("คำนำหน้าของคุณคือ เด็กชาย/เด็กหญิง ");
+        if (age >= 15 && age <= 19) {
+            System.out.println("วัยรุ่น");
+        } else if (age >= 20 && age <= 29) {
+            System.out.println("วัยหนุ่ม/สาว");
+        } else if (age >= 30 && age <= 39) {
+            System.out.println("วัยทำงาน");
+        } else if (age >= 40 && age <= 59) {
+            System.out.println("วัยกลางคน");
+        } else if (age >= 60) {
+            System.out.println("วัยชรา");
         }
     }
 }
