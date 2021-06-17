830e31e53d326b510bc801b59107f056f2fa3c17
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-17 21:30:42 +0700

    100

--- Self learning/Ifstatement.java
+++ Self learning/Ifstatement.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Ifstatement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("ป้อนอายุของคุณ : ");
+        System.out.print("ป้อนอายุของคุณ : ");
         int age = scanner.nextInt();
 
         if (age >= 15) {
