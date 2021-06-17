121175d331ff5f265c5089a58d26c16557b543d8
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-17 23:01:09 +0700

    100

--- Self learning/NestedIf.java
+++ Self learning/NestedIf.java
@@ -6,16 +6,16 @@ public class NestedIf {
         System.out.print("ป้อนอายุของคุณ : ");
         int age = scanner.nextInt();
         if (age <= 15) {
-            System.out.println("ม.ต้น");
             if (age == 15) {
                 System.out.println("ม.3");
             } else if (age == 14) {
                 System.out.println("ม.2");
             } else if (age == 13) {
                 System.out.println("ม.1");
+            } else {
+                System.out.println("ประถมศึกษา");
             }
         } else {
-            System.out.println("ม.ปลาย");
             if (age == 16) {
                 System.out.println("ม.4");
             } else if (age == 17) {
