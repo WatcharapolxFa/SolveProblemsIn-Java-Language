396348666b5f2b9d084c96e373163c8b9adbc754
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-19 23:09:56 +0700

    0

---
+++ Self learning/Assigment3.java
@@ -0,0 +1,10 @@
+import org.graalvm.compiler.loop.MathUtil;
+import java.util.Scanner;
+
+public class Assigment3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+}
