a79fe71338bfb0733667dc9e93ef90e0097f1ace
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-19 23:02:57 +0700

    50

---
+++ Self learning/Assignment2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ป้อนชื่อตัวเลขที่ 1  =");
+        int number1 = scanner.nextInt();
+
+    }
+}
