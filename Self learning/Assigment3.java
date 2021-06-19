7c7eb9feba713b16b86d739a698da2eefdc83782
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-19 23:14:45 +0700

    100 pi

--- Self learning/Assigment3.java
+++ Self learning/Assigment3.java
@@ -1,9 +1,16 @@
-import org.graalvm.compiler.loop.MathUtil;
+
 import java.util.Scanner;
 
 public class Assigment3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("ป้อนตัวเลขที่คุณต้องการหา : ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + " >> เป็นเลขคู๋");
+        } else {
+            System.out.println(number + " >> เป็นเลขคี่");
+        }
 
     }
 
