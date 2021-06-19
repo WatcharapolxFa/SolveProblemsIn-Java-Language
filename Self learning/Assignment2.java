4b68b7c7d6030dcf9f23d2bd3fc2825ce6519cde
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-19 23:07:52 +0700

    100 PI

--- Self learning/Assignment2.java
+++ Self learning/Assignment2.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class Assignment2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("ป้อนชื่อตัวเลขที่ 1  =");
+        System.out.print("ป้อนชื่อตัวเลขที่ 1  = ");
         int number1 = scanner.nextInt();
+        System.out.print("ป้อนชื่อตัวเลขที่ 2 = ");
+        int number2 = scanner.nextInt();
+        if (number1 > number2) {
+            System.out.println(number1 + " มากกว่า " + number2);
+
+        } else if (number1 < number2) {
+            System.out.println(number2 + " มากกว่า " + number1);
+        } else {
+            System.out.println(number1 + " เท่ากับ " + number2);
+        }
 
     }
 }
