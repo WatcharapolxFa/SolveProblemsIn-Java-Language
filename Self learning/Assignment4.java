31f3dd7fddbd28409f71702ef12cdda2bf0a608f
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-20 18:15:04 +0700

    100

--- Self learning/Assignment4.java
+++ Self learning/Assignment4.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Assignment4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("ป้อนตัวเลขลำดับที่ 1 : ");
-        int number01 = scanner.nextInt();
-        System.out.print("ป้อนตัวเลขลำดับที่ 2 : ");
-        int number02 = scanner.nextInt();
+        System.out.print("ป้อนตัวเลขที่คุณต้องการหา :  ");
+        int number = scanner.nextInt();
+        String sum = "";
+        sum = (number % 2 == 0) ? number + " >> เป็นเลขคู่" : number + " >> เป็นเลขคี่";
+        System.out.println(sum);
 
     }
 }
