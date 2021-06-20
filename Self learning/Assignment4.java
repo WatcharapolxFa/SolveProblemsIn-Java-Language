d3d01749ff95ca1d543135e68a15d39f7eab1c71
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-20 15:37:13 +0700

    50

--- Self learning/Assignment4.java
+++ Self learning/Assignment4.java
@@ -1,3 +1,12 @@
-public class Assignment4 {
+import java.util.Scanner;
 
+public class Assignment4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ป้อนตัวเลขลำดับที่ 1 : ");
+        int number01 = scanner.nextInt();
+        System.out.print("ป้อนตัวเลขลำดับที่ 2 : ");
+        int number02 = scanner.nextInt();
+
+    }
 }
