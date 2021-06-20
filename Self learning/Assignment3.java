cc36d9e573bcad0e2d23320d34218c66b9467b83
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-20 15:33:02 +0700

    0

--- Self learning/Assignment3.java
+++ Self learning/Assignment3.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Assigment3 {
+public class Assignment3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("ป้อนตัวเลขที่คุณต้องการหา : ");
