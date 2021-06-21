c34c57be65312546234c76dfde9b75ec8a3bc99b
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-21 17:00:49 +0700

    100pi

---
+++ Self learning/Assignment5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Assignment5 {
+    public static void main(String[] args) {
+        // แปลง พ.ศ. เป็น ค.ศ.
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+}
