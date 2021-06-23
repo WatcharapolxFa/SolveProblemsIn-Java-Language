e74113c23f78077a99f83af41683897fc736b768
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-23 16:02:29 +0700

    100 pi

---
+++ Self learning/Multiply.java
@@ -0,0 +1,13 @@
+public class Multiply {
+    public static void main(String[] args) {
+
+        for (int m = 2; m <= 12; m++) {
+            System.out.println("แม่ที่  = " + m);
+            for (int x = 1; x <= 12; x++) {
+                System.out.println(m + " X " + x + " = " + (m * x));
+
+            }
+        }
+
+    }
+}
