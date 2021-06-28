cd7f0c8fcef77d8dd8d9b10dfdf54affc9349358
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-06-28 23:05:18 +0700

    100

---
+++ Self learning/MinmaxLoops.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MinmaxLoops {
+    private static int numbers;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int maxnumber = 0, minnumber = 0;
+        while (true) {
+            System.out.print("ป้อนตัวเลข >> ");
+            int number = scanner.nextInt();
+            if (number < 0) {
+                break;
+            }
+            if (number > maxnumber)
+                maxnumber = number;
+
+            if (number < minnumber)
+                minnumber = number;
+
+        }
+        System.out.println("ค่ามากสุด >> " + maxnumber);
+        System.out.println("ค่าน้อยสุด >> " + minnumber);
+    }
+}
