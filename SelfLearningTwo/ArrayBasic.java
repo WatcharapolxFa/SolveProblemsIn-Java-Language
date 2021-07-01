580cc8031d79a3767cb26b553eb57024998bbc6f
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-07-01 22:28:14 +0700

    100

--- SelfLearningTwo/ArrayBasic.java
+++ SelfLearningTwo/ArrayBasic.java
@@ -1,3 +1,11 @@
 public class ArrayBasic {
+    public static void main(String[] args) {
+        int[] number = new int[4];
+        number[0] = 10;
+        number[1] = 20;
+        number[2] = 30;
+        number[3] = 40;
+        System.out.println(number[3]);
+    }
 
 }
