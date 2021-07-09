fd1a6a74d33cb817704191207ba38563201bb708
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-07-09 22:35:41 +0700

    100

---
+++ SelfLearningTwo/Array2miti.java
@@ -0,0 +1,8 @@
+public class Array2miti {
+    public static void main(String[] args) {
+        String[][] product = { { "ที่นอน", "หมอน", "ผ้าปู" }, { "แป้นพิมพ์", "คอมพิวเตอร์", "ที่วางมือ" },
+                { "แว่นตา", "กล่อง", "ขวดน้ำ" } };
+        System.out.println(product[2][1]);
+    }
+
+}
