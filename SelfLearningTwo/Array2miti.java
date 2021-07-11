f2e5bd777f8c8c761c6fed63b6af77f6ec175364
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-07-11 23:04:23 +0700

    100

--- SelfLearningTwo/Array2miti.java
+++ SelfLearningTwo/Array2miti.java
@@ -1,7 +1,7 @@
 public class Array2miti {
     public static void main(String[] args) {
         String[][] product = { { "ที่นอน", "หมอน", "ผ้าปู" }, { "แป้นพิมพ์", "คอมพิวเตอร์", "ที่วางมือ" },
-                { "แว่นตา", "กล่อง", "ขวดน้ำ" } };
+                { "แว่นตา", "กล่อง", "ขวดน้ำสิงห์" } };
 
         System.out.println(product[1][2]);
         product[2][2] = "แผ่นรองเมาส์";
