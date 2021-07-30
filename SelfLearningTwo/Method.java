87308b764c7c9c999a979c03f12e15b9fe46fdac
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-07-31 04:14:24 +0700

    100

--- SelfLearningTwo/Method.java
+++ SelfLearningTwo/Method.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class Method {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            Display();
-        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        // เรียกใช้ Display
+        Display();
+
+        // เรียกใช้ display
+        String name = scanner.nextLine();
+        display(name);
 
     }
 
     // แบบแรก
     static void Display() {
         // กลุ่มคำสั่งที่อยากให้ทำงาน
-        System.out.println("Hello world");
+        System.out.print("โปรป้อนชื่อจริง ของนายท่าน = > ");
+
+    }
+
+    // แบบสอง
+    static void display(String message) {
+        // กลุ่มคำสั่งที่อยากให้ทำงาน
+        System.out.println("ชื่อจริงของนายท่าน คือ > " + message + " ครับ / ค่ะ ");
 
     }
 
