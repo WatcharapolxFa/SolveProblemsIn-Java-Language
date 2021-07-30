6a2f4af216f5cbfa9298eda945205b5b2b8d4910
WatcharapolxFa <watcharapolyotadee@outlook.com>
2021-07-31 03:59:11 +0700

    100

---
+++ SelfLearningTwo/Method.java
@@ -0,0 +1,16 @@
+public class Method {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            Display();
+        }
+
+    }
+
+    // แบบแรก
+    static void Display() {
+        // กลุ่มคำสั่งที่อยากให้ทำงาน
+        System.out.println("Hello world");
+
+    }
+
+}
