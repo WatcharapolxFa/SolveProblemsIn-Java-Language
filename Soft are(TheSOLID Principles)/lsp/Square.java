797f2f75df5d619af01a7405f1af5ffd4afe1e3f
WatcharapolxFa <watcharapolyotadee@outlook.com>
2022-10-19 23:01:29 +0700

    Source

--- Soft are(TheSOLID Principles)/lsp/Square.java
+++ Soft are(TheSOLID Principles)/lsp/Square.java
@@ -1,22 +1,20 @@
-package com.solid.lsp;
+public class Square extends Shape {
 
-public class Square extends Rectangle {
+    private int side;
 
     Square(int side) {
         this.setSide(side);
     }
 
-    @Override
-    public void setWidth(int width) {this.setSide(width);
-    }
-
-    @Override
-    public void setHeight(int height) {
-        this.setSide(height);
-    }
-
     public void setSide(int side) {
-        super.setWidth(side);
-        super.setHeight(side);
+        this.side = side;
+    }
+
+    public int getSide() {
+        return this.side;
+    }
+
+    public int getArea() {
+        return side * side;
     }
 }
